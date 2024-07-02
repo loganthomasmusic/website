1967db2f6a4cd4a3d472f7aac7ca42a4cd2f9014
Aram Hammoudeh <aram.hammoudeh@indr.com>
2024-07-02 14:50:12 -0600

    refactor: Update ProfilePicture border width and hover scale

--- src/libs/design-system/ProfilePicture.jsx
+++ src/libs/design-system/ProfilePicture.jsx
@@ -3,10 +3,10 @@ import { styled } from "@mui/material/styles";
 export const ProfilePicture = styled("img")({
   objectFit: "cover",
   borderRadius: "50%",
-  border: "5px solid #333",
+  border: "2px solid #333",
   boxShadow: "0 0 10px 0 #333",
   transition: "all 0.3s",
   "&:hover": {
-    transform: "scale(1.1)",
+    transform: "scale(1.017)",
   },
 });
